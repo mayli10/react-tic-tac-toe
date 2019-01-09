30c1401ab90397396d7829df91db5c919d896c25
mayli10 <mali@vassar.edu>
2019-01-09 13:09:01 -0600

    Completed "Click Makes X #2" step
    Finished Step #6

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -10,16 +10,32 @@ class Square extends React.Component {
   }
     render() {
       return (
-        <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+        <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
         </button>
       );
     }
   }
 
 class Board extends React.Component {
+constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
   renderSquare(i) {
-    return <Square value={i}/>;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
@@ -43,10 +59,9 @@ class Board extends React.Component {
           {this.renderSquare(7)}
           {this.renderSquare(8)}
         </div>
-      </div>
-    );
+      );
+    }
   }
-}
 
 class Game extends React.Component {
   render() {
